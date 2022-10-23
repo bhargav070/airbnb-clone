44cf03f28c45d7321275421504f009abe5b0ad1a
bhargav <bhargavsompura07@gmail.com>
2022-10-23 12:27:35 +0530

    commited

---
+++ src/Components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <div className='main container'>
+        <img className='photo-grid' src= {require("../images/photo-grid.png")} alt='phtot-grid'/>
+        <h2 className='display'>Online Experiences</h2>
+        <p>Join unique interactive activities led <br></br>by one-of-a-kind hosts-all without leaving home</p>
+    </div>
+  )
+}
+
+export default Main
---
+++ src/Components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav className='container-fluid'>
+        <img src= {require(".../public/images/logo.png")} alt=''/>
+    </nav>
+  )
+}
